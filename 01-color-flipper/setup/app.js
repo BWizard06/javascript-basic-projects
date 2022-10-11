f1e99a12c70557cb2dd17891dd05d860081651ea
BWizard06 <89217401+BWizard06@users.noreply.github.com>
2022-10-11 20:12:43 +0200

    color flipper finished

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () =>{
+    const randomNumber = getRandonNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.innerHTML = colors[randomNumber];
+});
+
+function getRandonNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
